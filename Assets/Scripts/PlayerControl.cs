037bc90fe506dd9d448fbc195bce38eb0158c846
ludfiwahyu <ludfiwahyuh@gmail.com>
2020-08-24 12:17:20 +0700

    Add Player

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public KeyCode upButton = KeyCode.W;
+    public KeyCode downButton = KeyCode.S;
+    public float speed = 10f;
+    public float yBoundary = 9f;
+
+    public Rigidbody2D rigidBody2D;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 velocity = rigidBody2D.velocity;
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;
+        }
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }
+        else
+        {
+            velocity.y = 0f;
+        }
+        rigidBody2D.velocity = velocity;
+
+        Vector3 posisi = transform.position;
+        if (posisi.y > yBoundary)
+        {
+            posisi.y = yBoundary;
+        }
+        else if (posisi.y < yBoundary)
+        {
+            posisi.y = -yBoundary;
+        }
+        transform.position = posisi;
+    }
+
+    public void IncreamentScore()
+    {
+        score++;
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    public int Score
+    {
+        get { return score; }
+    }
+
+}
